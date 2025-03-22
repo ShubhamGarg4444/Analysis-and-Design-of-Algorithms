0dd87202f300251799d793cdc3b686ecd32089d7
Shubham <shubham2005garg@gmail.com>
2025-03-22 14:59:45 +0530

    prim correction

--- Prim's Algorithm/program.cpp
+++ Prim's Algorithm/program.cpp
@@ -1,98 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int prim(int n, vector<pair<pair<int, int>, int>> &g) 
-{
-    // Create adjacency list
-    unordered_map<int, list<pair<int, int>>> adj;
-
-    for (int i = 0; i < g.size(); i++) 
-    {
-        int u = g[i].first.first;  // Removed -1 adjustment
-        int v = g[i].first.second; // Removed -1 adjustment
-        int w = g[i].second;
-        adj[u].push_back(make_pair(v, w));
-        adj[v].push_back(make_pair(u, w));
-    }
-
-    vector<int> key(n, INT_MAX);   // Minimum distance between two adjacent nodes
-    vector<bool> mst(n, false);    // Track visited nodes
-    vector<int> parent(n, -1);     // Parent of each node in MST
-
-    // Start from node 0
-    key[0] = 0;
-    parent[0] = -1;
-
-    int mincost = 0;
-    int edgecount = 0; // Number of edges in MST
-
-    for (int i = 0; i < n - 1; i++) 
-    {
-        // Find the node with the minimum key value
-        int mini = INT_MAX;
-        int u = -1;
-
-        for (int v = 0; v < n; v++) {
-            if (!mst[v] && key[v] < mini) 
-            {
-                u = v;
-                mini = key[v];
-            }
-        }
-
-        if (u == -1) {
-            cout << "MST not possible (graph is disconnected)\n";
-            return -1;
-        }
-
-        // Mark the node as visited
-        mst[u] = true;
-        if (parent[u] != -1) 
-        {
-            mincost += key[u];
-            edgecount++;
-        }
-
-        // Update key values of adjacent nodes
-        for (auto it : adj[u]) {
-            int v = it.first;
-            int w = it.second;
-
-            if (!mst[v] && w < key[v]) {
-                key[v] = w;
-                parent[v] = u;
-            }
-        }
-    }
-
-    // Ensure MST is valid
-    if (edgecount != n - 1) {
-        cout << "MST not possible (graph is disconnected or has fewer edges)\n";
-        return -1;
-    }
-
-    // Print the MST edges
-    cout << "Edges in MST:\n";
-    for (int i = 1; i < n; i++) { 
-        cout << parent[i] << " -- " << i << " = " << key[i] << endl;
-    }
-
-    return mincost;
+void printMST(vector<int> &parent, vector<vector<int>> &graph) {
+    cout << "Edge \tWeight\n";
+    for (int i = 1; i < graph.size(); i++)
+        cout << parent[i] << " - " << i << " \t" << graph[parent[i]][i] << " \n";
 }
 
-int main() 
+void primMST(vector<vector<int>> &graph) 
 {
-    int n = 5;  
+    
+    int V = graph.size();
+  
+    // Key values used to pick minimum weight edge 
+    vector<int> key(V);
 
-    vector<pair<pair<int, int>, int>> edges = 
-{
-    {{0,1},2}, {{0,3},6}, {{1,2},3}, {{1,3},8}, {{1,4},5}, {{4,2},7}, {{3,4},4}
-};
+    // To represent set of vertices included in MST
+    vector<bool> mstSet(V);
 
-    int cost = prim(n, edges);
-    if (cost != -1) {
-        cout << "Minimum Spanning Tree cost: " << cost << endl;
+    // Array to store constructed MST
+    vector<int> parent(V);
+
+    // Initialize all keys as INFINITE
+    for (int i = 0; i < V; i++)
+        key[i] = INT_MAX, mstSet[i] = false;
+
+    // Always include first 1st vertex in MST.
+    // Make key 0 so that this vertex is picked as first
+    // vertex.
+    key[0] = 0;
+  
+    // First node is always root of MST
+    parent[0] = -1 ;
+
+    // The MST will have V vertices
+    for (int count = 0; count < V - 1; count++) {
+        
+        // Pick the minimum key vertex from the set of vertices not yet included in MST
+        int u ;
+        int mini = INT_MAX ;
+
+        for(int i=0;i<V;i++)
+        {
+            if(!mstSet[i] && key[i] < mini)
+            {
+                mini = key[i] ;
+                u = i ;
+            }
+        }
+
+        // Add the picked vertex to the MST Set
+        mstSet[u] = true;
+
+        // Update key value and parent index of the adjacent vertices of the picked vertex.
+        // Consider only those vertices which are not yet included in MST
+        for (int v = 0; v < V; v++)
+            {
+                if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
+                parent[v] = u ;
+                key[v] = graph[u][v] ;
+            }
     }
+
+    printMST(parent, graph);
+}
+
+// Driver's code
+int main() {
+      vector<vector<int>> graph = { { 0, 2, 0, 6, 0 },
+                                { 2, 0, 3, 8, 5 },
+                                { 0, 3, 0, 0, 7 },
+                                { 6, 8, 0, 0, 9 },
+                                { 0, 5, 7, 9, 0 } };
+
+    primMST(graph);
 
     return 0;
 }
